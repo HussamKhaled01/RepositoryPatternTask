69d68e44c01a1af59860ef9a08a9bf398440e81c
Hussa <Hussa@HUSSAM>
2026-03-24 04:44:37 +0300

    Setup Project Files + Create Db without tables

---
+++ RepositoryPatternTask/Data/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RepositoryPatternTask.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
+        {            
+        }
+
+
+    }
+}
